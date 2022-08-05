d8e2b45d70d8da750539075650f827f5ab23a4ca
Alexcey <alexcey007@yandex.ru>
2022-08-06 00:25:51 +0300

    add downcase for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,16 @@
 class User < ApplicationRecord
   has_secure_password
 
-  before_save :downcase_nickname
+  before_validation :downcase_nickname, :downcase_email
 
   validates :nickname, presence: true, uniqueness: true, length: { maximum: 40 }, format: { with: /\A[a-z\d_]+\z/ }
   validates :email, presence: true, uniqueness: true, format: {with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i}
   # наверно, стоит библиотку использовать URI::MailTo::EMAIL_REGEXP
   def downcase_nickname
     nickname.downcase!
+  end
+
+  def downcase_email
+    email.downcase!
   end
 end
