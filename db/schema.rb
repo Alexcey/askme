aa9c2eb91e9ebeede2093c2b21c00a7035528bb0
Alexcey <alexcey007@yandex.ru>
2022-08-08 18:47:55 +0300

    add header color on header view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_03_232943) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_07_210054) do
   create_table "questions", force: :cascade do |t|
     t.text "body", null: false
     t.integer "user_id"
@@ -28,6 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_03_232943) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "header_color", default: "#370617"
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["nickname"], name: "index_users_on_nickname", unique: true
   end
 
